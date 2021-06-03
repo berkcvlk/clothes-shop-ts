620421c80bb913bb01afe3a553f4e53b025d7a54
Berk Civelek <berkcvlk@gmail.com>
2021-06-04 02:30:32 +0300

    Logo created

---
+++ src/components/UI/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import { Wrapper, LogoText } from "./styles";
+import Wardrobe from "../Icons/Wardrobe";
+
+const Logo = () => {
+  return (
+    <Wrapper>
+      <Wardrobe />
+      <LogoText>wardrobe</LogoText>
+    </Wrapper>
+  );
+};
+
+export default Logo;
---
+++ src/components/UI/Logo/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const LogoText = styled.span`
+  color: var(--primary-color);
+
+  font-size: 21px;
+  font-weight: 600;
+
+  margin-left: 20px;
+`;
