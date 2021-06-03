a2bfdbcb28a6c6235425601290dddab48967ce6c
Berk Civelek <berkcvlk@gmail.com>
2021-06-04 02:31:54 +0300

    Main created

---
+++ src/layouts/Main/Main.tsx
@@ -0,0 +1,17 @@
+import Title from "../../components/UI/Title";
+import { Wrapper, Content } from "./styles";
+
+interface Props {
+  title: string;
+}
+
+const Main: React.FC<Props> = ({ children, title }) => {
+  return (
+    <Wrapper>
+      <Title content={title}>{title}</Title>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
+};
+
+export default Main;
---
+++ src/layouts/Main/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+  padding-top: 5px;
+`;
+
+export const Content = styled.div`
+  padding-top: 15px;
+`;
