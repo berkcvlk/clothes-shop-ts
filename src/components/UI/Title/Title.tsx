b9d8f66aa72186b5084dd9cf431b336cbb90c9cf
Berk Civelek <berkcvlk@gmail.com>
2021-06-04 02:31:41 +0300

    Title created

---
+++ src/components/UI/Title/Title.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Title = styled.h1<{ content: string }>`
+  position: relative;
+  width: 100%;
+
+  font-size: 25px;
+  font-weight: 700;
+  text-transform: lowercase;
+
+  &:before {
+    position: absolute;
+    margin: auto;
+    left: -55px;
+    top: -30px;
+    bottom: 0;
+
+    color: rgba(211, 211, 211, 0.12);
+    font-size: 60px;
+    font-weight: 800;
+
+    content: "${({ content }) => content}";
+  }
+`;
+
+export default Title;
