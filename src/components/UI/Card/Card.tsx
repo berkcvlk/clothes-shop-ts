d3836644b18279da72c1846eb49870514eba1850
Berk Civelek <berkcvlk@gmail.com>
2021-06-04 02:32:19 +0300

    Card created

---
+++ src/components/UI/Card/Card.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  background-color: #f8f8f8;
+  border-radius: 12px;
+`;
+
+export default Card;
