2ad70f64439ee98abb0a2cbb8c00309429e45bbd
Berk Civelek <berkcvlk@gmail.com>
2021-06-05 01:28:23 +0300

    Navigation refactored

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,9 +4,7 @@ import { Wrapper } from "./styles";
 const Navigation = () => {
   return (
     <Wrapper>
-      <NavigationLink to="/" exact>
-        Home
-      </NavigationLink>
+      <NavigationLink to="/" exact>Home</NavigationLink>
       <NavigationLink to="/favorites">Favorites</NavigationLink>
       <NavigationLink to="/cart">Cart</NavigationLink>
     </Wrapper>
