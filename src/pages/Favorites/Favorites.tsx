0b5647d9afc33a4bdfc417dacccc739fa976b710
Berk Civelek <berkcvlk@gmail.com>
2021-06-05 01:20:10 +0300

    Product list rendered on pages

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,7 +1,12 @@
+import ProductList from "../../components/ProductList";
 import Main from "../../layouts/Main";
 
 const Favorites = () => {
-  return <Main title="Favorites">Favorites Page</Main>;
+  return (
+    <Main title="Favorites">
+      <ProductList />
+    </Main>
+  );
 };
 
 export default Favorites;
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,7 +1,12 @@
+import ProductList from "../../components/ProductList/ProductList";
 import Main from "../../layouts/Main";
 
 const Products = () => {
-  return <Main title="New Collection">Product Page</Main>;
+  return (
+    <Main title="New Collection">
+      <ProductList />
+    </Main>
+  );
 };
 
 export default Products;
