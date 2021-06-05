786248e28414935e23bfc28bbe9a7ff8680f441f
Berk Civelek <berkcvlk@gmail.com>
2021-06-06 02:24:12 +0300

    Cart page styled

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,30 @@
+import styled from "styled-components";
+
 import Main from "../../layouts/Main";
 
 const Cart = () => {
-  return <Main title="Shopping Cart">Cart Page</Main>;
+  return (
+    <Main title="Shopping Cart">
+      <Wrapper>
+        <CartListContainer />
+        <TotalContainer />
+      </Wrapper>
+    </Main>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+
+  height: 200px;
+`;
+
+const CartListContainer = styled.div`
+  flex: 75%;
+`;
+
+const TotalContainer = styled.div`
+  flex: 25%;
+`;
 
 export default Cart;
