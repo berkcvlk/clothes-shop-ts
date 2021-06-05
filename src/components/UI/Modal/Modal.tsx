707b77ad767ba6544c8fed9ab7e9610b8058e014
Berk Civelek <berkcvlk@gmail.com>
2021-06-06 02:21:20 +0300

    Modal created

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,7 +1,35 @@
 import { createPortal } from "react-dom";
+import { useHistory } from "react-router";
 
-const Modal = () => {
-  return <div></div>;
+import Close from "../Icons/Close";
+import { Wrapper, Backdrop, Content, CloseIconButton } from "./styles";
+
+const Modal: React.FC = ({ children }) => {
+  const history = useHistory();
+
+  const goBackHandler = () => {
+    // Make the document scrollable again
+    document.body.style.overflowY = "scroll";
+    history.goBack();
+  };
+
+  const ModalRender = (
+    <>
+      <Backdrop onClick={goBackHandler} />
+      <Wrapper>
+        <Content>
+          <CloseIconButton onClick={goBackHandler}>
+            <Close />
+          </CloseIconButton>
+          {children}
+        </Content>
+      </Wrapper>
+    </>
+  );
+
+  return (
+    <>{createPortal(ModalRender, document.getElementById("modal-root")!)}</>
+  );
 };
 
 export default Modal;
---
+++ src/components/UI/Modal/styles.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import IconButton from "../Button/IconButton/IconButton";
+
+export const Wrapper = styled.div`
+  position: fixed;
+  top: 200px;
+  left: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+
+  width: 890px;
+  height: 565px;
+
+  padding: 45px;
+
+  background-color: white;
+  border-radius: 15px;
+`;
+
+export const Content = styled.div`
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgba(170, 170, 170, 0.7);
+  backdrop-filter: blur(3px);
+`;
+
+export const CloseIconButton = styled(IconButton)`
+  position: absolute;
+  top: 0;
+  right: 0;
+
+  z-index: 1040;
+`;
