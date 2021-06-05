530aef4f623f7f4e0bde1ddf6b6c49500ce5565f
Berk Civelek <berkcvlk@gmail.com>
2021-06-06 02:24:46 +0300

    IconButton created

---
+++ src/components/UI/Button/IconButton/IconButton.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Button from "../Button";
+
+const IconButton = styled(Button)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 1px;
+  border-radius: 15px;
+`
+export default IconButton;
