34636b1d7151cdb573f9c06b217ada4474147851
Berk Civelek <berkcvlk@gmail.com>
2021-06-06 02:23:09 +0300

    Added id for each route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Products, Favorites, Cart } from "../pages";
 
 export interface IRoute {
+  id: number;
   path: string;
   isExact: boolean;
   component?: JSX.Element;
@@ -8,16 +9,19 @@ export interface IRoute {
 
 const routes: IRoute[] = [
   {
+    id: 1,
     path: "/favorites",
     isExact: false,
     component: <Favorites />,
   },
   {
+    id: 2,
     path: "/cart",
     isExact: false,
     component: <Cart />,
   },
   {
+    id: 3,
     path: "/",
     isExact: true,
     component: <Products />,
