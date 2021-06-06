b43057d27ba4f07606d28a046c99be2ef8fdb43a
Berk Civelek <berkcvlk@gmail.com>
2021-06-06 16:12:20 +0300

    Total added on Cart page

--- src/components/Cart/Total/Total.tsx
+++ src/components/Cart/Total/Total.tsx
@@ -1,6 +1,14 @@
+import Button from "../../UI/Button";
+import { SpanTotal, TotalPrice, Wrapper } from "./styles";
+
 const Total = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <SpanTotal>Total</SpanTotal>
+      <TotalPrice>$349.95</TotalPrice>
+      <Button theme="primary">Proceed to Checkout</Button>
+    </Wrapper>
+  );
 };
 
 export default Total;
-
---
+++ src/components/Cart/Total/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  background-color: #f8f8f8;
+
+  border-radius: 12px;
+  padding: 25px 30px;
+`;
+
+export const SpanTotal = styled.span`
+  font-size: 20px;
+  color: #a7a7a7;
+  font-weight: 800;
+  text-transform: lowercase;
+`;
+
+export const TotalPrice = styled.span`
+  font-size: 32px;
+  font-weight: 600;
+
+  margin-bottom: 15px;
+`;
