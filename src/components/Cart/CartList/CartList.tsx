6e98f326416043f98902e9c35af5033e7bd8c2e1
Berk Civelek <berkcvlk@gmail.com>
2021-06-06 16:12:55 +0300

    CartList created

--- src/components/Cart/CartList/CartList.tsx
+++ src/components/Cart/CartList/CartList.tsx
@@ -1,5 +1,14 @@
-const CartList = () => {
-  return <div></div>;
-};
+import CartListItem from "./CartListItem";
+import TableHeaders from "./Table/TableHeaders";
 
+const CartList = () => {
+  return (
+    <>
+      <TableHeaders />
+      {[...new Array(4)].map((e, i) => (
+        <CartListItem key={i} />
+      ))}
+    </>
+  );
+};
 export default CartList;
