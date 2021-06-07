e7d6f610fe4f78a01d161e1a4cae3ca077e901c5
Berk Civelek <berkcvlk@gmail.com>
2021-06-07 03:20:20 +0300

    Types file renamed

---
+++ src/types/index.ts
@@ -0,0 +1,32 @@
+export interface IProduct {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+}
+
+export interface IProductContext {
+  items: IProduct[];
+  getById: (id: string) => IProduct;
+}
+
+export interface IFavoriteContext {
+  items: IProduct[];
+  add: (id: string) => void;
+  remove: (id: string) => void;
+  isFavorite: (id: string) => boolean;
+}
+
+export interface ICartItem {
+  id: string;
+  title: string;
+  price: number;
+  amount: number;
+}
+
+export interface ICartContext {
+  items: ICartItem[];
+  total: number;
+  add: (i: ICartItem) => void;
+  remove: (id: string) => void;
+}
