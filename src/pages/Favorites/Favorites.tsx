4cb7787267ab73558f6739eecdcc7958139bd701
Berk Civelek <berkcvlk@gmail.com>
2021-06-07 03:19:53 +0300

    Context added to pages

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
+
+import { FavoriteContext } from "../../contexts/Favorite";
 import ProductList from "../../components/Product/ProductList";
 import Main from "../../layouts/Main";
 
 const Favorites = () => {
+  const { items } = useContext(FavoriteContext);
+
   return (
     <Main title="Favorites">
-      <ProductList />
+      <ProductList list={items} fav={true} />
     </Main>
   );
 };
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
+import { ProductContext } from "../../contexts/Product";
+
 import ProductList from "../../components/Product/ProductList";
 import Main from "../../layouts/Main";
 
 const Products = () => {
+  const { items } = useContext(ProductContext);
+
   return (
     <Main title="New Collection">
-      <ProductList />
+      <ProductList list={items} />
     </Main>
   );
 };
