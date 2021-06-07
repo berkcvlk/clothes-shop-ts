7974c4b643412ea7575f99fb0541f7620b575304
Berk Civelek <berkcvlk@gmail.com>
2021-06-07 03:23:23 +0300

    Context added to Amount

--- src/components/Cart/CartList/CartListItem/Amount/Amount.tsx
+++ src/components/Cart/CartList/CartListItem/Amount/Amount.tsx
@@ -1,14 +1,32 @@
-import { Add, Delete } from "../../../../UI/Icons";
+import { useContext } from "react";
+
+import { CartContext } from "../../../../../contexts/Cart";
+import { ICartItem } from "../../../../../types";
+
+import { Add, Delete, Minus } from "../../../../UI/Icons";
 import { Count, DecrementButton, IncrementButton, Wrapper } from "./styles";
 
-const Amount = () => {
+interface Props {
+  item: ICartItem;
+}
+
+const Amount: React.FC<Props> = ({ item }) => {
+  const { add, remove } = useContext(CartContext);
+  const { id, amount } = item;
+
+  const onAdd = () => add(item);
+  const onRemove = () => remove(id);
+
+  // Make the decrement button toggle
+  // according to amount. If it's 1, icon will be
+  // trash bin, otherwise it'll be the dash
+  const decrementSwitch = amount === 1 ? <Delete /> : <Minus />;
+
   return (
     <Wrapper>
-      <DecrementButton>
-        <Delete />
-      </DecrementButton>
-      <Count>1</Count>
-      <IncrementButton>
+      <DecrementButton onClick={onRemove}>{decrementSwitch}</DecrementButton>
+      <Count>{amount}</Count>
+      <IncrementButton onClick={onAdd}>
         <Add />
       </IncrementButton>
     </Wrapper>
