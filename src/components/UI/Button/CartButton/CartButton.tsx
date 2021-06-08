d0e5fd0927c48387018c87e93dd29c87edd006a8
Berk Civelek <berkcvlk@gmail.com>
2021-06-08 18:41:16 +0300

    Added button stage

--- src/components/UI/Button/CartButton/CartButton.tsx
+++ src/components/UI/Button/CartButton/CartButton.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 const CartButton: FC<Props> = ({ item: { id, title, price } }) => {
   // To add item to the cart list
-  const { add } = useContext(CartContext);
+  const { add, stage } = useContext(CartContext);
 
   const onAddItem = () => {
     add({
@@ -24,11 +24,22 @@ const CartButton: FC<Props> = ({ item: { id, title, price } }) => {
     });
   };
 
-  return (
-    <Button onClick={onAddItem} theme="primary">
-      Add to Cart
-    </Button>
-  );
+  // According to stage which can be only
+  // "added" and "initial", change button text
+  // and color. This can be changed, later.
+  // *style with inline can be changed, too.
+  const buttonRender =
+    stage === "initial" ? (
+      <Button onClick={onAddItem} theme="primary">
+        Add to Cart
+      </Button>
+    ) : (
+      <Button style={{ backgroundColor: "#76C893", color: "white" }}>
+        Added
+      </Button>
+    );
+
+  return buttonRender;
 };
 
 export default CartButton;
