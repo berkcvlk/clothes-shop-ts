75bbcf914da945a2eaf7168e8bdd3229b664a42c
Berk Civelek <berkcvlk@gmail.com>
2021-06-08 17:55:12 +0300

    QuickView refactored

--- src/components/Product/QuickView/QuickView.tsx
+++ src/components/Product/QuickView/QuickView.tsx
@@ -1,15 +1,14 @@
 import { useParams } from "react-router";
 import { useContext } from "react";
 
-import { CartContext } from "../../../contexts/Cart";
 import { ProductContext } from "../../../contexts/Product";
 
-import Button from "../../UI/Button";
 import Modal from "../../UI/Modal";
+import FavoriteButton from "../../UI/Button/FavoriteButton/FavoriteButton";
+import CartButton from "../../UI/Button/CartButton/CartButton";
 import * as s from "./styles";
 
 import { images } from "../../../utils/image";
-import FavoriteButton from "../../UI/Button/FavoriteButton/FavoriteButton";
 
 const QuickView = () => {
   const { id } = useParams<{ id: string }>();
@@ -17,18 +16,6 @@ const QuickView = () => {
 
   // Item to render on quickview
   const { title, price, description } = getById(id);
-
-  // To add item to the cart list
-  const { add: addCartItem } = useContext(CartContext);
-
-  const onAddCartItem = () => {
-    addCartItem({
-      id: id,
-      title: title,
-      price: price,
-      amount: 1,
-    });
-  };
 
   return (
     <Modal>
@@ -41,9 +28,7 @@ const QuickView = () => {
           <s.Description>{description}</s.Description>
           <s.Price>{`$${price}`}</s.Price>
           <s.ButtonWrapper>
-            <Button theme="primary" onClick={onAddCartItem}>
-              Add to Cart
-            </Button>
+            <CartButton item={{ id, title, price }} />
             <FavoriteButton id={id} />
           </s.ButtonWrapper>
         </s.Detail>
